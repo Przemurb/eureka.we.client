b95dbf5b7e5aea14ce37fca01e186f1cdd51e61d
Przemurb <przemurb@hotmail.it>
2022-12-12 17:21:38 +0100

    Initial commit

---
+++ src/test/java/it/vito/eureka/we/client/EurekaWebAppTests.java
@@ -0,0 +1,13 @@
+package it.vito.eureka.we.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaWebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
